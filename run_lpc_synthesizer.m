0477683355b1290bacaf8a0de8336d06bed855a5
prismaticoder <pjjsalam1999@gmail.com>
2024-10-20 22:21:36 +0100

    update run file

--- run_lpc_synthesizer.m
+++ run_lpc_synthesizer.m
@@ -1,3 +1,4 @@
+% Female Speech Synthesizer
 synthesizer = LpcSpeechSynthesizer('heed_f.wav', 25, 'lpc');
 
 synthesizer.plotFrequencyResponses();
@@ -6,4 +7,19 @@ synthesizer.estimateFormantFrequencies();
 
 synthesizer.getMeanFundamentalFrequency();
 
+synthesizer.plotPoleZeroPlot();
+
 synthesizer.synthesize('heed_f_output.wav');
+
+% Male Speech Synthesizer
+synthesizer = LpcSpeechSynthesizer('heed_m.wav', 25, 'lpc');
+
+synthesizer.plotFrequencyResponses();
+
+synthesizer.estimateFormantFrequencies();
+
+synthesizer.getMeanFundamentalFrequency();
+
+synthesizer.plotPoleZeroPlot();
+
+synthesizer.synthesize('heed_m_output.wav');
